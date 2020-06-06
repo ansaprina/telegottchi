60e56dcfc3a7db698091d1d7177f779b6758f5f3
ansaprina <66479160+ansaprina@users.noreply.github.com>
2020-06-07 01:18:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ def normalize(i):
     if i > 100:
         i = 100
     return i
+
+
+class Event:
+
+    def __init__(self, event_type, name, happiness, health, satiety, energy):
+        self.type = event_type
+        self.name = name
+        self.happiness = happiness
+        self.health = health
+        self.satiety = satiety
+        self.energy = energy
